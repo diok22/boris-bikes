33f476987b406a1f36d0f5cbabb8be762c423670
Stuart Cooper <stucooper86@gmail.com>
2016-09-28 15:12:18 +0100

    ch10 relase bike + working rspec pass

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,6 +7,14 @@ describe DockingStation do
       expect(docking_station.class).to eq DockingStation
   end
 
+  it "release bike method creates an instance of the class" do
+    expect(subject.release_bike).to be_instance_of Bike
+  end
+
   it { is_expected.to respond_to(:release_bike) }
-  
+
+  it "takes a new bike and responds to working method" do
+    expect(Bike.new).to respond_to(:working?)
+  end
+
 end
